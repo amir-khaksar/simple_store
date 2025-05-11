73c3d616c42ac2a54a9fa7dc81880ac6bb71da9a
amir-khaksar <=>
2025-05-11 14:18:28 +0330

    add shopping card stucture

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import About from "./Pages/About/About";
 import AuthPage from "./Pages/Auth/AuthPage";
 import Index from "./Pages/Index/Index";
+import ShoppingCard from "./Pages/ShoppingCard/ShoppingCard";
 
 export const routes = [
    { path: "/", element: <Index /> },
    { path: "/auth", element: <AuthPage /> },
    { path: "/about", element: <About /> },
+   { path: "/shoppingCard", element: <ShoppingCard /> },
 ];
 
 export default routes;
